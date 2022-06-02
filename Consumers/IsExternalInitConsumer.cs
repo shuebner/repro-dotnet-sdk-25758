cc1aa967e690fd8c1f9f0b89fdbe488705f616b4
Sven Hübner <shuebner@qti.qualcomm.com>
2022-06-02 06:31:54 +0200

    init

---
+++ Consumers/IsExternalInitConsumer.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace GEM.VISA
+{
+    public record IsExternalInitConsumer(string Value);
+
+    public class NullableAttributeConsumer
+    {
+        object? _value;
+
+        [MemberNotNull(nameof(_value))]
+        public void Init()
+        {
+            _value = new();
+        }
+    }
+}
